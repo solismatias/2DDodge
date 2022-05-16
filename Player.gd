094d9e922798c93c7a1cd0f9bf06195383932c25
solismatias <solisma42@gmail.com>
2022-05-16 20:36:44 -0300

    Display Animation when moving

--- Player.gd
+++ Player.gd
@@ -6,6 +6,7 @@ var screen_size # Size of the game window.
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -27,9 +28,19 @@ func _process(delta):
 	else:
 		# $AnimatedSprite = get_node("AnimatedSprite")
 		$AnimatedSprite.stop()
+		
 	# delta refers to the frame length, the amount of time that the previous frame took to complete
 	# ensures that your movement will remain consistent
 	# even if the frame rate changes.
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	
+	# Choosing Animation
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
